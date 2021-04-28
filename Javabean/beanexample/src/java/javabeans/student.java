0fd20820edba2a331981185039fa641d468f50a6
Harit Patel <75656894+Clinckop@users.noreply.github.com>
2021-04-28 23:32:01 +0530

    Create student.java

---
+++ Javabean/beanexample/src/java/javabeans/student.java
@@ -0,0 +1,36 @@
+package javabeans;
+
+import java.io.Serializable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class student implements Serializable{
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int a) {
+        age = a;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    
+    
+}
